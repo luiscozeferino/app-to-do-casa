de342869dfc1e858098a358e644eab625c3be709
LUIS CARLOS DE OLIVEIRA ZEFERINO <luiscozeferino@gmail.com>
2023-07-06 23:52:07 -0300

    atualizacao exerciciosv4

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 //Interface
 import { TaskList } from '../../model/task-list';
 
@@ -7,12 +7,18 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
-  public taskList: Array<TaskList> = [
+export class TodoListComponent implements DoCheck {
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+
+
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
+  //public taskList: Array<TaskList> = [
     //{ task: "Minha nova Task", checked: true },
     //{ task: "Minha nova Task2", checked: false }
 
-  ];
+  //];
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
   }
@@ -28,6 +34,22 @@ export class TodoListComponent {
     this.taskList.push({ task: event, checked: false });
 
 
+  }
+  public validationInput(event: string, index: number) {
+
+    if (!event.length) {
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+
+    }
+  }
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
   }
 
 }
